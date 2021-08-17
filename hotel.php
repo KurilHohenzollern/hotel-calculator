9ef84a70c3be09bd81e62b2e94e0af8d59136e74
KurilHohenzollern <82318381+KurilHohenzollern@users.noreply.github.com>
2021-08-17 17:18:20 +0400

    add hotel.php
    hotel.php - веб-страничка, которая перенаправляла на страницы с нужными настройками.

---
+++ hotel.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <title>HTML5</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+
+ </head>
+ <body>
+    <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Интерфейс гостиницы</h1>
+          <p class="lead text-muted">Нажмёшь синюю кнопку — и сезону конец. Ты проснёшься в своей постели и поверишь, что это был сон. Нажмёшь серую кнопку — войдешь в страну чудес. Я покажу тебе, насколько дорога кроличья нора в сутки.</p>
+          <p>
+            <a href="reception.php" class="btn btn-primary my-2">Установить сезоны</a>
+            <a href="setRooms.php" class="btn btn-secondary my-2">Составить прейскурант</a>
+          </p>
+        </div>
+      </section>
+
+</body>
+</html>
